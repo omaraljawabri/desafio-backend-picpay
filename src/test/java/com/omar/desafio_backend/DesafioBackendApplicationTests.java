1ca8102472a41e2f02ba0e347adc6e293182c828
Omar <164200540+omaraljawabri@users.noreply.github.com>
2025-02-10 09:26:51 -0300

    init project

---
+++ src/test/java/com/omar/desafio_backend/DesafioBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.omar.desafio_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
