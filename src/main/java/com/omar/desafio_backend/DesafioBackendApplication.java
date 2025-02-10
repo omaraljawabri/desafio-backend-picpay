269657af0d7e8d69718d0a520d1409e29b120810
Omar <164200540+omaraljawabri@users.noreply.github.com>
2025-02-10 11:40:41 -0300

    enable feign requests

--- src/main/java/com/omar/desafio_backend/DesafioBackendApplication.java
+++ src/main/java/com/omar/desafio_backend/DesafioBackendApplication.java
@@ -2,8 +2,10 @@ package com.omar.desafio_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class DesafioBackendApplication {
 
 	public static void main(String[] args) {
