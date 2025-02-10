93d362876d41b5f0fc4d6e26c2bd04946108855b
Omar <164200540+omaraljawabri@users.noreply.github.com>
2025-02-10 14:59:47 -0300

    Add http status to custom exceptions

--- src/main/java/com/omar/desafio_backend/exceptions/ExternalRequestErrorException.java
+++ src/main/java/com/omar/desafio_backend/exceptions/ExternalRequestErrorException.java
@@ -1,5 +1,9 @@
 package com.omar.desafio_backend.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_GATEWAY)
 public class ExternalRequestErrorException extends RuntimeException {
 
     public ExternalRequestErrorException(){
--- src/main/java/com/omar/desafio_backend/exceptions/UserNotFoundException.java
+++ src/main/java/com/omar/desafio_backend/exceptions/UserNotFoundException.java
@@ -1,5 +1,9 @@
 package com.omar.desafio_backend.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class UserNotFoundException extends RuntimeException {
 
     public UserNotFoundException(){
