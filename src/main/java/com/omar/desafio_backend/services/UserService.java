95edd453f4bfde379305133f54d024803bec9247
Omar <164200540+omaraljawabri@users.noreply.github.com>
2025-02-10 11:03:49 -0300

    remove unused imports

--- src/main/java/com/omar/desafio_backend/services/UserService.java
+++ src/main/java/com/omar/desafio_backend/services/UserService.java
@@ -10,13 +10,8 @@ import com.omar.desafio_backend.repositories.UserRepository;
 import jakarta.validation.ValidationException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.web.PagedModel;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 @Service
 @RequiredArgsConstructor
